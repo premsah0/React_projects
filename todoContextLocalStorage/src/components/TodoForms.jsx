9eeb72187affa286cb80ae84ccc9a3ba86eb2527
Prem Sah <iamprem2004@gmail.com>
2026-02-02 23:59:24 +0530

    change

--- todoContextLocalStorage/src/components/TodoForms.jsx
+++ todoContextLocalStorage/src/components/TodoForms.jsx
@@ -9,17 +9,24 @@ function TodoForm() {
     e.preventDefault();
     if (!todo) return;
 
-    addTodo({todo, completed: false });
-    setTodo("")
+    addTodo({ todo, completed: false });
+    setTodo("");
   };
   return (
     <form onSubmit={add} className="flex">
+      <input
+        type="checkbox"
+        value=""
+        className="sr-only peer"
+        onChange={onChangeBtn}
+        checked={themeMode === "dark"}
+      />
       <input
         type="text"
         placeholder="Write Todo..."
         className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
         value={todo}
-        onChange={(e)=>setTodo(e.target.value)}
+        onChange={(e) => setTodo(e.target.value)}
       />
       <button
         type="submit"
--- todoContextLocalStorage/src/contexts/TodoContexts.js
+++ todoContextLocalStorage/src/contexts/TodoContexts.js
@@ -21,4 +21,3 @@ export const useTodo = ()=>{
 }
 
 export const TodoProvider = TodoContext.Provider
- 
